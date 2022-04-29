f01ee720a81ae50eeb64810ae168b113e940fb24
wjp <wjp@qq.com>
2022-04-30 00:14:34 +0800

    master commit

--- src/main/java/com/gk/git_test/GitTest.java
+++ src/main/java/com/gk/git_test/GitTest.java
@@ -3,5 +3,8 @@ package com.gk.git_test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("master test");
+
     }
+
 }
